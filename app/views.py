652525ac9a2f3777d0a5ff25fdda93aab4910187
dlehman14 <davidlehman14@github.com>
2017-10-27 23:35:22 -0400

    First ISHARLibrary page layout

--- app/views.py
+++ app/views.py
@@ -5,3 +5,8 @@ from flask import render_template, redirect, abort, url_for, jsonify
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/ishar-library/')
+def ishar_library():
+    return render_template('ISHARLibrary.html')
